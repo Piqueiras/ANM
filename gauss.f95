cf232c7194f36edfc0100cfa186a13927048b433
Piqueras <96251034+Piqueiras@users.noreply.github.com>
2023-03-29 13:21:06 +0200

    LU

--- gauss.f95
+++ gauss.f95
@@ -1,3 +1,5 @@
+!Diversas subrutinas relacionadas con la reducción de Gauss
+
 subroutine gauss(n,A,b,deter)
     !Subrutina que emplea reducción de Gauss para convertir la matriz en triangular superior
     !En el resto de elementos se guardan los factores de multiplicado, así que la matriz resultante se va a tener que calcular siempre como triangular superior
@@ -33,3 +35,25 @@ subroutine gauss(n,A,b,deter)
     enddo
     deter = deter * a(n,n)
 end subroutine gauss
+
+subroutine gauss_LU(n,A)
+    !Subrutina anterior modificada para simplemente calcular la LU, que encaja en la matriz original
+    use mod_clreal
+    implicit none
+    integer,intent(in)::n
+    real(clreal),intent(inout)::A(n,n)
+    integer :: i,j,k
+    real(clreal) :: z, eps
+    eps = 1.e-12
+
+    do k=1,n-1
+        if ( abs(a(k,k)) < eps ) then
+            stop "Elemento diagonal nulo"
+        end if
+        do i=k+1,n !Iteramos por filas
+            z=a(i,k)/a(k,k) !z va a ser el factor de multiplicación       
+            A(i,k+1:n)=A(i,k+1:n)-z*A(k,k+1:n) !Así se hace más facil la transformación
+            a(i,k)=z !Guardamos el factor de multiplicación. Por si acaso. Total, la otra opción era ponerlo a 0.
+        enddo
+    enddo
+end subroutine gauss_LU
