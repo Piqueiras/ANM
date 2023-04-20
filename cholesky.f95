e374953b2709a5be79d1b6226bc88181beadb9ce
Piqueras <96251034+Piqueiras@users.noreply.github.com>
2023-04-20 16:22:38 +0200

    Cholesky resumido

--- cholesky.f95
+++ cholesky.f95
@@ -7,19 +7,15 @@ subroutine cholesky(n,A)
     integer :: i,j,k
 
     do j=1,n
-        if ( a(j,j) <= 0 ) then
-            stop "Matriz no definida positiva"
-        end if
         !Esquina
-        do k = 1, j-1
-            a(j,j)=a(j,j)-a(j,k)*a(j,k)
-        end do
+        a(j,j)=a(j,j)-sum(a(j,1:j-1)*a(j,1:j-1))
+        if ( a(j,j) <= 0 ) then
+            stop "Matriz no definida positiva (o no es simétrica)"
+        end if
         a(j,j)=SQRT(a(j,j))
         !Resto columna
         do i=j+1,n
-            do k = 1, j-1
-                a(i,j)=a(i,j)-a(i,k)*a(j,k)
-            end do
+            a(i,j)=a(i,j)-sum(a(i,1:j-1)*a(i,1:j-1))
             a(i,j)=a(i,j)/a(j,j)
             a(j,i)=0
         enddo
